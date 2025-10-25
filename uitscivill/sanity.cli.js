a85c556ef28d350a7f596289d0b69432c99d6cf0
shariar-opu <126817974+shariar-opu@users.noreply.github.com>
2025-10-25 19:45:22 +0600

    feat: Add appId to deployment configuration in Sanity CLI

--- uitscivill/sanity.cli.js
+++ uitscivill/sanity.cli.js
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'mj4o5n8m2fjqibylbs6ltria',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
